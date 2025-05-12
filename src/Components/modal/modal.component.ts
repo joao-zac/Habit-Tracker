2af21ac468ff5f76dfcba4ad6549620ac0247b52
joao-zac <102718142+joao-zac@users.noreply.github.com>
2025-05-12 18:51:51 -0300

    :sparkles: feat: logica do formulario

--- src/Components/modal/modal.component.ts
+++ src/Components/modal/modal.component.ts
@@ -1,9 +1,18 @@
+import { CommonModule } from '@angular/common';
 import { Component, EventEmitter, Output } from '@angular/core';
+import {
+  FormControl,
+  FormGroup,
+  Validators,
+  ReactiveFormsModule,
+  FormArray,
+} from '@angular/forms';
+import { IDays, arrayDays } from '../../utils/days';
 
 @Component({
   selector: 'app-modal',
   standalone: true,
-  imports: [],
+  imports: [ReactiveFormsModule, CommonModule],
   templateUrl: './modal.component.html',
   styleUrl: './modal.component.scss',
 })
@@ -13,4 +22,24 @@ export class ModalComponent {
   closeModal(): void {
     this.close.emit();
   }
+
+  protected dias: IDays[] = arrayDays;
+
+  protected form = new FormGroup(
+    Object.fromEntries(
+      this.dias.map((option) => [
+        option.id,
+        new FormControl(false, { nonNullable: true }),
+      ])
+    )
+  );
+
+  get selectedValues() {
+    return Object.keys(this.form.value).filter((key) => this.form.value[key]);
+  }
+
+  // onFormSubmit() {
+  //   console.log('Habito Form');
+  //   console.log(this.habitForm.value);
+  // }
 }
---
+++ src/utils/days.ts
@@ -0,0 +1,14 @@
+export interface IDays {
+  dia: String;
+  id: number;
+}
+
+export const arrayDays: IDays[] = [
+  { dia: 'Seg', id: 1 },
+  { dia: 'Ter', id: 2 },
+  { dia: 'Qua', id: 3 },
+  { dia: 'Qui', id: 4 },
+  { dia: 'Sex', id: 5 },
+  { dia: 'Sáb', id: 6 },
+  { dia: 'Dom', id: 7 },
+];
